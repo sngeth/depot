9a13def172027dbfacb42ed3a994ca073996b06a
Sid Ngeth <sngeth@gmail.com>
2017-06-05 13:49:10 -0400

    Add a user resource

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 class OrderMailerTest < ActionMailer::TestCase
   test "received" do
     mail = OrderMailer.received(orders(:one))
-    byebug
     assert_equal "Pragmatic Store Order Confirmation", mail.subject
     assert_equal ["dave@example.org"], mail.to
     assert_equal ["depot@example.com"], mail.from
